12e1237fb58bdc44ffbe86c6eb28da606af3f0c2
Albundy73 <albino.pereira@free.fr>
2026-08-23 17:16:53 +0200

    Remove manual ingest control

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import ClubPulseDashboard from "@/components/clubpulse-dashboard";
-import IngestButton from "@/components/ingest-button";
 
 export default function Home() {
-  return (
-    <>
-      <ClubPulseDashboard />
-      <IngestButton />
-    </>
-  );
+  return <ClubPulseDashboard />;
 }
